7cd6594ad9916fc5681091dc6d599ed7b50d1c96
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-01-04 15:30:03 +0100

    partie 1

--- tpFontion.py
+++ tpFontion.py
@@ -1,41 +1,34 @@
 import re
+
 nom = input("indiquez votre nom")
 prenom = input("indiquez votre prenom")
 annee = int(input("indique votre annee de naissance"))
-mail = input("indiquez votre adresse mail\n")
 
-if annee > 2016:
+if annee > 2022 - 12:
     print("vous etes Poussin")
-elif annee > 2010:
+elif annee > 2022 - 18:
     print("vous etes Cadet")
-elif annee > 2004:
-    print("vous n'etes pas majeur")
-elif annee > 1998:
+elif annee > 2022 - 24:
     print("vous etes Junior")
-elif annee > 1992:
+elif annee > 2022 - 30:
     print("vous etes Semi-pro")
-elif annee >= 1982:
+elif annee >= 2022 - 40:
     print("vous etes pro")
 else:
     print("Non admis")
 
+
 def verif_mail(adresse):
-    rex = re.compile("[A-Za-z0-9]{4}@[A-Za-z0-9]{6}\.[A-Z|a-z]{3}")
+    rex = re.compile("[A-Z]{1}.[a-z]{7}@baton-rouge.fr")
     if rex.fullmatch(adresse):
         return True
     else:
         return False
 
-def verif_mail_long(adresse):
-    if adresse[2] != "." or adresse[11] != "@":
-        return False
-    for ind, val in enumerate(adresse):
-        if (ind < 2) or (ind >= 1 and ind <= 10) or ind > 11:
-            if not val.isalnum():
-                return False
-    return True
 
-if verif_mail_long(mail):
-    print("OK")
+mail = input("indiquez votre adresse mail\n")
+
+if verif_mail(mail):
+    print("mail enregistrer")
 else:
-    print("KO")
+    print("mail incorrect")
