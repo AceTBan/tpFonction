264df69b523ff4430ab280851514a699470b2d14
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-01-04 16:56:05 +0100

    la bonne save de 17h

--- tpFontion.py
+++ tpFontion.py
@@ -1,8 +1,18 @@
+# coding: utf-8
 import re
 
-nom = input("indiquez votre nom")
-prenom = input("indiquez votre prenom")
-annee = int(input("indique votre annee de naissance"))
+nom = input("indiquez votre nom \n")
+prenom = input("indiquez votre prenom \n")
+annee = int(input("indique votre annee de naissance \n"))
+
+
+def verif_mail(adresse):
+    rex = re.compile('[A-Z]{1}.[a-z]{7}@baton-rouge.fr')
+    if rex.fullmatch(adresse):
+        return True
+    else:
+        return False
+
 
 if annee > 2022 - 12:
     print("vous etes Poussin")
@@ -17,18 +27,9 @@ elif annee >= 2022 - 40:
 else:
     print("Non admis")
 
-
-def verif_mail(adresse):
-    rex = re.compile("[A-Z]{1}.[a-z]{7}@baton-rouge.fr")
-    if rex.fullmatch(adresse):
-        return True
-    else:
-        return False
-
-
 mail = input("indiquez votre adresse mail\n")
 
 if verif_mail(mail):
-    print("mail enregistrer")
+    print("mail accepter")
 else:
     print("mail incorrect")
