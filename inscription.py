660bd6e0c154e924ed062c0dcc871ba37f6ee012
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-01-05 12:20:06 +0100

    modification du fichier .py (input/liste)

--- inscription.py
+++ inscription.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 import re
 
-nom = input("indiquez votre nom \n")
-prenom = input("indiquez votre prenom \n")
-annee = int(input("indique votre annee de naissance \n"))
-
+inscription = []
+inscription["nom"] = input("indiquez votre nom \n")
+inscription["prenom"] = input("indiquez votre prenom \n")
+inscription["annee"] = int(input("indique votre annee de naissance \n"))
 
 def verif_mail(adresse):
     rex = re.compile('[A-Z]{1}.[a-z]{7}@baton-rouge.fr')
@@ -14,15 +14,15 @@ def verif_mail(adresse):
         return False
 
 
-if annee > 2022 - 12:
+if inscription["annee"] > 2022 - 12:
     print("vous etes Poussin")
-elif annee > 2022 - 18:
+elif inscription["annee"] > 2022 - 18:
     print("vous etes Cadet")
-elif annee > 2022 - 24:
+elif inscription["annee"] > 2022 - 24:
     print("vous etes Junior")
-elif annee > 2022 - 30:
+elif inscription["annee"] > 2022 - 30:
     print("vous etes Semi-pro")
-elif annee >= 2022 - 40:
+elif inscription["annee"] >= 2022 - 40:
     print("vous etes pro")
 else:
     print("Non admis")
