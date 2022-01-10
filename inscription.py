0ac53c706d58dcfcea27bc806ea4f2b7d66fe94f
AceTBan <esteban_david@ymail.com>
2022-01-10 12:31:44 +0100

    commit de 12h

--- inscription.py
+++ inscription.py
@@ -4,29 +4,29 @@ import fonction
 inscription = []
 nom = []
 prenom = []
-annee = []
+
 
 inscrits = int(input("Combien d'adhérent voulez vous saisir ?\n"))
 
 for i in range(inscrits):
     nom.append(input("indiquez le nom de l'adherent\n"))
     prenom.append(input("indiquez le prenom de l'adherent\n"))
-    annee.append(input("indique l'annee de naissance de l'adherent\n"))
+    annee = int(input("indique l'annee de naissance de l'adherent\n"))
 
 age = 2022-annee
 
-if age > 12:
-    print("vous etes en catégorie Poussin")
-elif age > 18:
-    print("vous etes en catégorie Cadet")
-elif age > 24:
-    print("vous etes en catégorie Junior")
-elif age > 30:
-    print("vous etes en catégorie Semi-pro")
-elif age >= 40:
-    print("vous etes en catégorie pro")
+if age < 12:
+    print("l'adherent en catégorie Poussin")
+elif age < 18:
+    print("l'adherent en catégorie Cadet")
+elif age < 24:
+    print("l'adherent en catégorie Junior")
+elif age < 30:
+    print("l'adherent en catégorie Semi-pro")
+elif age <= 40:
+    print("l'adherent en catégorie pro")
 else:
-    print("Non admis")
+    print("l'adherent est Non admissible")
 
 mail = input("indiquez votre adresse mail\n")
 
